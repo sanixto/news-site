d765c75cf82ed0af81716a850c6126ec6314d992
sanixto <sanix.to@gmail.com>
2024-05-01 17:22:53 +0300

    fetch news from another server using server

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,41 +1,19 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-
 import NewsList from '@/components/news-list';
 import News from '@/interfaces/news.interface';
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>();
-  const [news, setNews] = useState<News[]>();
+export default async function NewsPage() {
+  const host: string = process.env.HOST!;
+  const port: string = process.env.SERVER_PORT!;
+  const response: Response = await fetch(`http://${host}:${port}/news`);
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response: Response = await fetch('http://localhost:8080/news');
+  if (!response.ok) throw new Error('Failed to fetch news.');
 
-      if (!response.ok) {
-        setError('Failed to fetch news.');
-        setIsLoading(false);
-      }
-
-      const news: News[] = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>
-
-  if (error) return <p>{error}</p>
+  const news: News[] = await response.json();
 
   return (
     <>
       <h1>News page</h1>
-      {news && <NewsList news={news} />}
+      {<NewsList news={news} />}
     </>
   );
 }
